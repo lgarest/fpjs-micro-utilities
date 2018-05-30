b2833f82d0c3f54ea92c75d8d5dd4ad5168c48a3
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-05-30 12:52:02 +0200

    Add eslint-airbnb linter

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ const { apply, unapply } = require('./apply-unapply');
 const { negate, isEven, isOdd } = require('./predicate');
 const { compose, pipe } = require('./compose-pipe');
 const { binary, unary } = require('./unary-binary');
+
+export default {
+  apply,
+  binary,
+  compose,
+  isEven,
+  isOdd,
+  negate,
+  pipe,
+  unapply,
+  unary,
+};
